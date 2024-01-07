56f497e6586a05f1c5c7ae483e73d9285323eaf3
Viktoria Jechsmayr <6342890+vJechsmayr@users.noreply.github.com>
2024-01-07 15:40:19 +0100

    setup server.js
    Initial Backend Function and RSS Reader

---
+++ backend/route/homeSRV.js
@@ -0,0 +1,18 @@
+module.exports = {
+    setup: function(app, db, sendResponse){
+        // app.get('/home/layout', (req, res) =>{
+        //     const origin = '/home/layout';
+        //     let query = 'SELECT * FROM layouts';
+
+        //     db.query(query, (err, result, fields)=>{
+        //         if(err){
+        //             sendResponse(origin, res, false, 'Fehler beim Abfragen der Layout-Daten! \n' + err + '\n' + err.message + '\n' + query);
+        //         }else{
+        //             sendResponse(origin, res, true, '', result);
+        //         }
+        //     });
+        // });
+
+
+    }
+};
